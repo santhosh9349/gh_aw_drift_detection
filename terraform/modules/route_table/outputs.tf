54cdd0e9a842c5c1e4a77b378e18da17e5e5c1fe
santhosh <santhosh93499@gmail.com>
2026-04-25 20:47:00 +0100

    Remove scaling example and Transit Gateway connectivity guide; delete route tables and Transit Gateway configuration files; clean up related modules and variables for a streamlined Terraform setup.

--- terraform/modules/route_table/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "route_table_id" {
-  description = "The ID of the route table"
-  value       = aws_route_table.this.id
-}
-
-output "route_table_arn" {
-  description = "The ARN of the route table"
-  value       = aws_route_table.this.arn
-}
